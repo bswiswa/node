bed3be1df9e123ca3d84182214653d1ed8fac022
Batsirai Swiswa <batsiraiswiswa@yahoo.com>
2017-12-12 20:17:30 -0500

    Clean up sandbox

--- sandbox/app.js
+++ sandbox/app.js
@@ -1,26 +1,6 @@
 console.log("Starting app");
 
 const fs = require("fs");
-const os = require("os");
-const notes = require("./notes.js");
 const _ = require("lodash");
 
-
-console.log(_.isString(true));
-console.log(_.isString("true"));
-let filteredArray = _.uniq("Batsiraii1234");
-console.log(filteredArray);
-
-//let result = notes.addNote();
-//console.log(result);
-//
-//let add = notes.add(9,2);
-//console.log(add);
-//
-//let user = os.userInfo();
-//
-//fs.appendFile("greetings.txt", `Hello ${user.username}, you are ${notes.age}!`, function(err){
-//    if(err){
-//        console.log("Unable to write to file");
-//    }
-//});
+const notes = require("./notes.js");
--- sandbox/notes.js
+++ sandbox/notes.js
@@ -4,8 +4,3 @@ module.exports.addNote = ()=>{
     console.log("adding note");
     return "a note";
 };
-
-module.exports.add = (a, b)=>{
-    console.log(`adding ${a} and ${b}`);
-    return a+b;
-};
