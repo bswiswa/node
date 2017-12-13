092b6746ccf2778861b0931e673699d033b9ff31
Batsirai Swiswa <batsiraiswiswa@yahoo.com>
2017-12-12 21:50:44 -0500

    Reading in command line arguments into our application by accessing the process.argv array

--- sandbox/app.js
+++ sandbox/app.js
@@ -4,3 +4,22 @@ const fs = require("fs");
 const _ = require("lodash");
 
 const notes = require("./notes.js");
+
+//
+let command = process.argv[2];
+console.log("Command: "+ command);
+
+if(command === "add"){
+    console.log("adding new note...");
+}else if(command === "list"){
+    console.log("listing all notes...");
+}else if(command === "read"){
+    console.log("reading note...");
+}else if(command === "remove"){
+    console.log("removing note...")
+}
+else{
+    console.log("command not recognized");
+}
+
+console.log(process.argv);
