b8af1338f2f19971e39006ad34fa9fc10a5add03
Batsirai Swiswa <batsiraiswiswa@yahoo.com>
2017-12-15 13:40:04 -0500

    Make HTTP requests and extract longitude and latitude

--- weather-app/app.js
+++ weather-app/app.js
@@ -5,8 +5,9 @@ request({
     url: "https://maps.googleapis.com/maps/api/geocode/json?address=4%20Burr%20Street%20New%20Haven%20CT",
     json: true
 }, (error, response, body)=>{
-    console.log(JSON.stringify(body, undefined, 2));
-    /*
-    JSON.stringify takes the object, and an object specifying the fields you want to filter, last is an integer specifying the number of spaces for indentation
-    */
+    if(response.statusCode == 200){
+        let address = body.results[0].formatted_address;
+        let location = body.results[0].geometry.location;
+        console.log(`The address is ${address}. The longitude and latitude of this address are ${location.lat} and ${location.lng}`);
+    }
 });
