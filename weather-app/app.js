70427fc372ac3a72f3c186ae487a137d17b8b7a3
Batsirai Swiswa <batsiraiswiswa@yahoo.com>
2017-12-16 07:56:05 -0500

    Error handling in weather-app

--- weather-app/app.js
+++ weather-app/app.js
@@ -22,13 +22,13 @@ request({
     url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
     json: true
 }, (error, response, body)=>{
-    if(response.statusCode == 200){
-        if(body.results[0]){
+        if(error){
+            console.log("Unable to connect to Google servers");
+        } else if(body.status === "ZERO_RESULTS"){
+            console.log("Unable to find that address");
+        }else if(response.statusCode === 200){
             let address = body.results[0].formatted_address;
             let location = body.results[0].geometry.location;
             console.log(`Address: ${address}\nLongitude: ${location.lng}\nLatitude: ${location.lat}`);
-        }else{
-            console.log("No result found, modify address");
         }
-    }
 });
