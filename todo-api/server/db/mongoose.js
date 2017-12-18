d208823b932f91ce079b74c56715a897a0ff24e4
Batsirai Swiswa <batsiraiswiswa@yahoo.com>
2017-12-18 15:22:36 -0500

    Refactor code to separate models, server and mongoose db scripts, Create POST /todos route

---
+++ todo-api/server/db/mongoose.js
@@ -0,0 +1,12 @@
+//Mongoose configuration and export
+
+let mongoose = require("mongoose");
+
+//Mongoose supports promises or callbacks. We want to use the promises we have been using before not some other third party Promises library.
+mongoose.Promise = global.Promise;
+//note that the global object is above the process and it encloses it ie global.process === process
+
+//connect to database
+mongoose.connect("mongodb://localhost:27017/TodoApp");
+
+module.exports = { mongoose };
--- todo-api/server/server.js
+++ todo-api/server/server.js
@@ -1,58 +1,23 @@
-let mongoose = require("mongoose");
-//Mongoose supports promises or callbacks. We want to use the promises we have been using before not some other third party Promises library.
-mongoose.Promise = global.Promise;
-//note that the global object is above the process and it encloses it ie global.process === process
+let express = require("express");
+let bodyParser = require("body-parser");
 
-//connect to database
-mongoose.connect("mongodb://localhost:27017/TodoApp");
+let { mongoose } = require("./db/mongoose");
+let { Todo } = require("./models/todo");
+let { User } = require("./models/user");
 
-//create a model so Mongoose knows how to store our data
-let Todo = mongoose.model("Todo", 
-{
-    text: {
-        type: String,
-        required: true,
-        minlength: 1,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    }, completedAt: {
-        type: Number,
-        default: null
-    }
+let app = express();
+
+//bodyParser takes JSON and converts it into an object and attaches it to the body of the request
+app.use(bodyParser.json());
+
+//configuring routes
+//CRUD - create, read, update, delete
+app.post("/todos", (request, response) => {
+    let todo = new Todo({ text: request.body.text });
+    todo.save().then((result)=>{ response.send(result)}, (err)=> { response.status(400).send(err)});
 });
 
-let User = mongoose.model("User", {
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 1
-    }   
+app.listen(3000, () => {
+    console.log("App started on port 3000");
 });
-
-let names = ["shaima", "vamoyo", "vincent", "rugare", "tadiwa"];
-
-for(let i = 0 ; i < 5; i++){
-    let user = new User({ email: "   " });
-    user.save().then((result)=>{console.log(`User added\n ${JSON.stringify(result, undefined, 2)}`)}, (err)=>{ console.log("Failed to add user", err)});
-}
-
-////create an instance
-//let aTodo = new Todo({ text: " Edit "});
-//
-//aTodo.save().then((result)=>{
-//   console.log(`Saved todo ${result}`) 
-//}, (err)=> {
-//   console.log("Unable to save Todo", err); 
-//});
-
-//let tasks = ["Take out trash", "Clean house", "Hang up Christmas tree lights", "Study", "Read for pleasure"];
-//for(let i=0; i < 5; i++){
-//   let todo = new Todo({ text: tasks[i], completed: (i%2 == 0)? true: false});
-//    todo.save().then((result)=>{console.log(`todo saved successfully ${JSON.stringify(result, undefined, 2)}`);}, (err)=>{ console.log("Could not save todo", err)});
-//}
-
             
