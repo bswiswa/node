009139d4ce62b38f0d93608ca3b0d6a680b72424
Batsirai Swiswa <batsiraiswiswa@yahoo.com>
2017-12-18 10:24:51 -0500

    Searching by ObjectId

---
+++ todo-api/playground/mongodb-find.js
@@ -0,0 +1,13 @@
+const { MongoClient, ObjectID } = require("mongodb");
+
+MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, database) =>{
+    if(err){
+        return console.log("Unable to connect to MongoDB server");
+    }
+    const db = database.db("TodoApp");
+    db.collection("Todos").find({ _id: new ObjectID("5a37cc67291fb336089dbd62") }).toArray().then((docs) =>{
+        console.log("Todos");
+        console.log(JSON.stringify(docs, undefined, 2));
+    }, (err)=> { console.log("Unable to fetch todos", err)});
+//    database.close();
+});
