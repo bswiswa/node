91c12a052971959c261e48f87af06c7e3f06051f
Batsirai Swiswa <batsiraiswiswa@yahoo.com>
2017-12-19 07:12:39 -0500

    Add GET /todos/:id

--- todo-api/server/server.js
+++ todo-api/server/server.js
@@ -4,6 +4,7 @@ let bodyParser = require("body-parser");
 let { mongoose } = require("./db/mongoose");
 let { Todo } = require("./models/todo");
 let { User } = require("./models/user");
+const { ObjectID } = require("mongodb");
 
 let app = express();
 
@@ -24,6 +25,19 @@ app.get("/todos", (request, response) => {
    }, (err)=> { 
        response.status(400).send(err);
     });
+});
+
+//URL parameter :name.  Creates a name variable on the request.params object
+app.get("/todos/:id", (request, response)=> {
+    let id = request.params.id;
+    if(!ObjectID.isValid(id)){
+      return response.status(404).send();  
+    }
+    Todo.findById(id).then(todo => {
+        if(!todo) return response.status(404).send();
+        response.send(todo);
+    }).catch(e => response.status(400).send());
+    //leave out sending error back because it may contain sensitive information
 });
 
 app.listen(3000, () => {
