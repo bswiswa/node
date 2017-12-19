b33694234ec097b9de298425d36eb9ef700ed118
Batsirai Swiswa <batsiraiswiswa@yahoo.com>
2017-12-19 06:52:34 -0500

    Learn Mongoose queries

--- todo-api/playground/mongoose-queries.js
+++ todo-api/playground/mongoose-queries.js
@@ -1,10 +1,12 @@
 //play around with querying with Mongoose
 const { mongoose } = require("./../server/db/mongoose");
 const { Todo } = require("./../server/models/todo");
+const { User } = require("./../server/models/user")
 const { ObjectID } = require("mongodb");
 
 //a valid object ID which may or may not be in database
-let id = "5a3867c6315e62c205bdd3e611";
+let id = "5a3867c6315e62c205bdd3e6";
+
 
 //can use the isValid method to check format of id before searching
 if(!ObjectID.isValid(id)) return console.log("ID is not valid");
@@ -28,3 +30,19 @@ Todo.findById(id).then((todo)=>{
    console.log("Todo findFindById", todo); 
 }).catch(e => console.log(e));
 
+
+let userId = "5a380919341257293a0e4263";
+User.find({ _id: userId}).then(users =>{
+    if(users.length < 1) return console.log("unable to find user", userId);
+    console.log("users array", users);
+} ).catch(e => console.log(e));
+
+User.findOne({ _id: userId }).then(user=> {
+    if(!user) return console.log("unable to find user", userId);
+    console.log("userObj", user);   
+} ).catch(e => console.log(e));
+
+User.findById({ _id: userId}).then(user =>{
+    if(!user) return console.log("unable to find user", userId);
+    console.log("user", user);
+} ).catch(e => console.log(e));
