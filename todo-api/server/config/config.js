504d6ee71704509bd07001061f8e1c643245b479
Batsirai Swiswa <batsiraiswiswa@yahoo.com>
2017-12-23 13:11:37 -0500

    Update config to use config.json, set production secret key with heroku config:set

--- todo-api/server/config/config.js
+++ todo-api/server/config/config.js
@@ -2,10 +2,19 @@
 //we need to configure it for our development and test databases
 let env = process.env.NODE_ENV || "development";
 
-if(env === "development"){
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = "mongodb://localhost:27017/TodoApp";
-}else if(env === "test"){
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = "mongodb://localhost:27017/TodoAppTest"
+if(env === "development" || env === "test"){
+    //when JSON is required, it is automatically parsed
+    let config = require("./config.json");
+    let envConfig = config[env];
+    Object.keys(envConfig).forEach(key => {
+        process.env[key] = envConfig[key];
+    });
 }
+
+//if(env === "development"){
+//    process.env.PORT = 3000;
+//    process.env.MONGODB_URI = "mongodb://localhost:27017/TodoApp";
+//}else if(env === "test"){
+//    process.env.PORT = 3000;
+//    process.env.MONGODB_URI = "mongodb://localhost:27017/TodoAppTest"
+//}
--- todo-api/server/models/user.js
+++ todo-api/server/models/user.js
@@ -42,7 +42,7 @@ UserSchema.methods.toJSON = function () {
 UserSchema.methods.generateAuthToken = function(){
   let user = this;
     let access = "auth";
-    let token = jwt.sign({_id: user._id.toHexString(), access}, "abc123" ).toString();
+    let token = jwt.sign({_id: user._id.toHexString(), access}, process.env.JWT_SECRET).toString();
     user.tokens.push({access, token});
     return user.save().then(()=> token);
 };
@@ -58,7 +58,7 @@ UserSchema.statics.findByToken = function(token){
     let decoded;
     //jwt throws an error if something goes wrong
     try{
-       decoded = jwt.verify(token, "abc123");
+       decoded = jwt.verify(token, process.env.JWT_SECRET);
     }catch(e){
         return Promise.reject();
     }
