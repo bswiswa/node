6faae10de4f3fb289a8ca7b4eab632e4c666dd49
Batsirai Swiswa <batsiraiswiswa@yahoo.com>
2017-12-26 12:42:31 -0500

    Error handling in async-await using e.message

--- async-await/currency-convert.js
+++ async-await/currency-convert.js
@@ -1,14 +1,26 @@
 const axios = require("axios");
 
 const getExchangeRate =  async (from, to) => {
-    const response = await axios.get(`https://api.fixer.io/latest?base=${from}`);
-    return response.data.rates[to];
+    
+    try{
+      const response = await axios.get(`https://api.fixer.io/latest?base=${from}`);
+    if(response.data.rates[to])
+        return response.data.rates[to]; 
+    else
+        throw new Error();
+    }catch(e){
+        throw new Error(`Unable to get exchange rate for ${from} to ${to}.`);
+    } 
 };
 
 //get countries that accept a given currency
 const getCountries = async (currencyCode) => {
-    const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`);
-    return response.data.map(country => country.name);
+    try {
+        const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`);
+        return response.data.map(country => country.name);
+    }catch(e){
+       throw new Error(`Unable to get countries that use the currency code ${currencyCode}`);
+    } 
 };
 
 
@@ -37,4 +49,4 @@ const convertCurrencyAlt = async (from, to, amount) => {
 
 convertCurrency("ZAR", "USD", 100).then(conversion => console.log(conversion));
 
-convertCurrencyAlt("USD", "EUR", 100).then(conversion => console.log(conversion));
+convertCurrencyAlt("USD", "EUR", 100).then(conversion => console.log(conversion)).catch(e => console.log(e.message));
